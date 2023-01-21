832743e57cc75c82d73aa4ce32f0ddc8684f7f1f
Dmitry <mangosovdima@mail.ru>
2023-01-21 13:59:15 +0300

    task4

---
+++ Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Нахождение палиндрома числа 
+Console.Write("Ведите число: ");
+int number = int.Parse(Console.ReadLine());
+int mod = 0;
+int newmod = 0;
+int i=0;
+for(i= number;number!=0;number=number/10)
+{
+    mod = number % 10;
+    newmod = newmod*10 + mod;
+}
+if(i == newmod) Console.WriteLine("Число палиндром");
+else Console.WriteLine("число не является палиндромом");
