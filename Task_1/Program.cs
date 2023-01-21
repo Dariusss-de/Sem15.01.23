88ec8326233a455374ecef66788086914eb50740
Dmitry <mangosovdima@mail.ru>
2023-01-21 13:57:55 +0300

    task1

---
+++ Task_1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Найти число которое делится на 7 и на 23 без остатка
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if(number%7==0 && number%23 ==0) Console.WriteLine("Верно");
+else Console.WriteLine("Неверно");
+
